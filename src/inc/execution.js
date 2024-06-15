108d6f9dcbe919114c410e611b6602c303632b23
Tung Mai <tungsekai0902@gmail.com>
2024-06-15 16:33:11 +0700

    Chay tren VM ubuntu

--- src/inc/execution.js
+++ src/inc/execution.js
@@ -2,20 +2,6 @@ import { execSync } from 'child_process';
 import db from '../inc/database.js';
 
 
-// function convertWindowsPathToWslPath(windowsPath) {
-// 	// Regular expression to match Windows drive letters followed by a colon
-// 	const driveLetterRegex = /([A-Z]):/g;
-
-// 	const wslPath = windowsPath.replace(driveLetterRegex, (match, driveLetter) => {
-// 		// Convert the drive letter to lowercase and prepend "/mnt/"
-// 		return `/mnt/${driveLetter.toLowerCase()}`;
-// 	});
-
-// 	// Replace backslashes with forward slashes
-// 	return wslPath.replace(/\\/g, '/');
-// }
-
-
 const MS_PER_SEC = 1e3
 const NS_PER_SEC = 1e9
 
@@ -56,24 +42,17 @@ export async function evaluateSubmission({ uuid, path: sourceFilePath }) {
 	//#region Chạy chương trình, với từng test case
 	TEST_CASES.forEach(c => {
 		const TEST_CASE_ID = c.id
-		let execCmd = `${EXE_PATH}`
+		let execCmd = `/root/ptrace-v2 ${EXE_PATH}`
 		let timeout = c.time_out
 		let testInput = c.input
 		let desiredOutput = c.output
 
-		// let timeRun = null
-
 		try {
-			// const TIME_START = process.hrtime()
-
 			const actualOutput = execSync(execCmd, {
 				timeout: timeout * MS_PER_SEC,
 				input: testInput + '\n',
 				encoding: 'utf-8'
 			})
-
-			// const TIME_DIFF = process.hrtime(TIME_START)
-			// timeRun = (TIME_DIFF[0] * NS_PER_SEC + TIME_DIFF[1]) / NS_PER_SEC
 
 			const RESULT = actualOutput.trim() == desiredOutput.trim()
 			TEST_RESULT[TEST_CASE_ID] = RESULT
@@ -92,8 +71,7 @@ export async function evaluateSubmission({ uuid, path: sourceFilePath }) {
 		}
 	})
 
-	// Tính điểm:
-	// Lấy thang điểm 10, chia cho tổng số case rồi nhân với số case đạt
+	// Tính điểm: Lấy thang điểm 10, chia cho tổng số case rồi nhân với số case đạt
 	const MAX_SCORE = 10
 	const SCORE = MAX_SCORE / TEST_CASES.length * Object.values(TEST_RESULT).filter(Boolean).length
 
