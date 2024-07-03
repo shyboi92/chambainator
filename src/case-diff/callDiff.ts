87d29ea6301f80e3b1c84804247269dbe31bb76a
shyboi92 <tungsekai0902@gmail.com>
2024-07-04 01:27:36 +0700

    xu ly return code =1 cua diff

--- src/case-diff/callDiff.ts
+++ src/case-diff/callDiff.ts
@@ -1,8 +1,17 @@
 import { execSync } from "child_process";
 
 export default function callDiff(pathFileCodeRoot: string, pathFileCodeCompare: string) {
-	const result = execSync(`diff -iwB ${pathFileCodeRoot} ${pathFileCodeCompare}`, {
-		encoding: "utf-8"
-	});
+	let result: string
+	try {
+		result = execSync(`diff -iwB ${pathFileCodeRoot} ${pathFileCodeCompare}`, { encoding: "utf-8" });
+	} catch (e) {
+		if (e.status == 1) {
+			result = e
+		} else {
+			console.error(e)
+			return null
+		}
+	}
+
 	return result.trim()
 }
